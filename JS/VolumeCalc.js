812610e7b27879a8bef68e4e1696ea50589b1baf
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-10-28 12:54:03 -0400

    anotherCommit
    Added VolumeCalc.

---
+++ JS/VolumeCalc.js
@@ -0,0 +1,6 @@
+
+var length = prompt('What is the length?');
+var width = prompt('What is the width?');
+var height = prompt('What is the height');
+
+console.log('The volume is: ' + (length*width*height*1));
--- JS/day1_variables.js
+++ JS/day1_variables.js
@@ -6,9 +6,14 @@
  */
 
 var name;
-name = 'Richie Arrigo'; // string example
+//name = 'Richie Arrigo'; // string example
+name = prompt('What is your name?'); // string example
+
 var age = 21; // number example
+age = prompt('How old are you?');
+
 var pet = 'Apollo';
+
 var petAge = 7;
 
 //console.log(name);
@@ -21,8 +26,21 @@ age = age * 2;
 console.log(age);
 */
 
-
 // Three ways to tell a string to be a number
 console.log(parseInt(petAge) + Number(age) + age*1);
+
+var outputString;
+outputString =
+'User age is : \t\t' + age + '\n' +
+'User name is : \t\t' + name + '\n' +
+'User Pet name is : \t' + pet + '\n' +
+'User Pet age is : \t' + petAge;
+
+console.log(outputString);
+
+console.log(age*1 + petAge);
+
+
+
 
 

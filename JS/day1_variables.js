694259ce71d2d21061c8c665735cbbc00de7b135
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-10-28 11:52:27 -0400

    numberCommit
    Creating and manipulating integers.

--- JS/day1_variables.js
+++ JS/day1_variables.js
@@ -6,7 +6,23 @@
  */
 
 var name;
-name = 'Richie Arrigo';
+name = 'Richie Arrigo'; // string example
+var age = 21; // number example
+var pet = 'Apollo';
+var petAge = 7;
 
-console.log(name);
+//console.log(name);
+/*console.log(age);
+age = age + 1;
+console.log(age);
+age = age - 10;
+console.log(age);
+age = age * 2;
+console.log(age);
+*/
+
+
+// Three ways to tell a string to be a number
+console.log(parseInt(petAge) + Number(age) + age*1);
+
 

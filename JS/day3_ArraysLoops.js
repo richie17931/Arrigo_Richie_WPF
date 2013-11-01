f9206c32deae1ecfa397f9baa9b0befc1d192a8f
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-01 15:45:30 -0400

    Day3SecondCommit

--- JS/day3_ArraysLoops.js
+++ JS/day3_ArraysLoops.js
@@ -23,49 +23,59 @@ console.log(arrayLayerOne[2][0]);
 console.log(arrayLayerOne[2][0][0]);
 */
 
-//seed value for 0,0
-//modifier to add to seed
+//seed- value for 0,0
+//modifier- added to seed
+var arrayLayer=[];
+var number=Number(prompt('Enter a number:'));
+var seed=Number(prompt('Enter a number to start at:'));
+var modifier=Number(prompt('Enter a modifier:'));
 
-//var arrayLayer=[];
-//var number=Number(prompt('Enter a number:'));
-//var seed=Number(prompt('Enter a number to start at:'));
-//var modifier=Number(prompt('Enter a modifier:'));
-//
+for(i=0;i<=3;++i){
+    arrayLayer[i]=[];
+}
 //arrayLayer[0]=[];
 //arrayLayer[1]=[];
 //arrayLayer[2]=[];
 //arrayLayer[3]=[];
-//
-//
-//arrayLayer[0][0]=seed;
-//arrayLayer[0][1]=seed+modifier;
-//arrayLayer[0][2]=seed+(modifier*2);
-//arrayLayer[0][3]=seed+(modifier*3);
-//
-//arrayLayer[1][0]=seed+modifier;
-//arrayLayer[2][0]=seed+(modifier*2);
-//arrayLayer[3][0]=seed+(modifier*3);
-//
-//arrayLayer[1][1]=arrayLayer[0][1]*arrayLayer[1][0]*number;//4
-//arrayLayer[1][2]=arrayLayer[0][2]*arrayLayer[1][0]*number;//6
-//arrayLayer[1][3]=arrayLayer[0][3]*arrayLayer[1][0]*number;//8
-//
-//arrayLayer[2][1]=arrayLayer[0][1]*arrayLayer[2][0]*number;//6
-//arrayLayer[2][2]=arrayLayer[0][2]*arrayLayer[2][0]*number;//9
-//arrayLayer[2][3]=arrayLayer[0][3]*arrayLayer[2][0]*number;//12
-//
-//arrayLayer[3][1]=arrayLayer[0][1]*arrayLayer[3][0]*number;//8
-//arrayLayer[3][2]=arrayLayer[0][2]*arrayLayer[3][0]*number;//12
-//arrayLayer[3][3]=arrayLayer[0][3]*arrayLayer[3][0]*number;//16
-//
-//arrayLayer[0][0]= arrayLayer[0][0]*number;
-//arrayLayer[0][1]= arrayLayer[0][1]*number;
-//arrayLayer[0][2]= arrayLayer[0][2]*number;
-//arrayLayer[0][3]= arrayLayer[0][3]*number;
-//arrayLayer[1][0]= arrayLayer[1][0]*number;
-//arrayLayer[2][0]= arrayLayer[2][0]*number;
-//arrayLayer[3][0]= arrayLayer[3][0]*number;
-//
+
+//for(i=0;i<4;i++){
+//    for(j=0;j<4;j++){
+//        console.log(arrayLayer[i][j])
+//        seed=seed+modifier;
+//    }
+//}
+arrayLayer[0][0]=seed;
+arrayLayer[0][1]=seed+modifier;
+arrayLayer[0][2]=seed+(modifier*2);
+arrayLayer[0][3]=seed+(modifier*3);
+
+arrayLayer[1][0]=seed+modifier;
+arrayLayer[2][0]=seed+(modifier*2);
+arrayLayer[3][0]=seed+(modifier*3);
+
+arrayLayer[1][1]=arrayLayer[0][1]*arrayLayer[1][0]*number;//4
+arrayLayer[1][2]=arrayLayer[0][2]*arrayLayer[1][0]*number;//6
+arrayLayer[1][3]=arrayLayer[0][3]*arrayLayer[1][0]*number;//8
+
+arrayLayer[2][1]=arrayLayer[0][1]*arrayLayer[2][0]*number;//6
+arrayLayer[2][2]=arrayLayer[0][2]*arrayLayer[2][0]*number;//9
+arrayLayer[2][3]=arrayLayer[0][3]*arrayLayer[2][0]*number;//12
+
+arrayLayer[3][1]=arrayLayer[0][1]*arrayLayer[3][0]*number;//8
+arrayLayer[3][2]=arrayLayer[0][2]*arrayLayer[3][0]*number;//12
+arrayLayer[3][3]=arrayLayer[0][3]*arrayLayer[3][0]*number;//16
+
+arrayLayer[0][0]= arrayLayer[0][0]*number;
+arrayLayer[0][1]= arrayLayer[0][1]*number;
+arrayLayer[0][2]= arrayLayer[0][2]*number;
+arrayLayer[0][3]= arrayLayer[0][3]*number;
+arrayLayer[1][0]= arrayLayer[1][0]*number;
+arrayLayer[2][0]= arrayLayer[2][0]*number;
+arrayLayer[3][0]= arrayLayer[3][0]*number;
+
+for(i=0;i<4;i++){
+    console.log(arrayLayer[i]);
+}
 //console.log(arrayLayer[0]);
 //console.log(arrayLayer[1]);
 //console.log(arrayLayer[2]);
@@ -80,16 +90,16 @@ console.log(arrayLayer[3][1]);
 console.log(arrayLayer[3][2]);
 console.log(arrayLayer[3][3]);
 */
-//
+
 //for(i=0;i<=2;i=i+1){
 //    for(j=0;j<=2;j++){
 //        console.log('[' +i +'][' +j +']');
 //    }
 //}
 
-var num=Number(prompt('Enter a number:'));
-
-for(i=num;i<=num*num;i=i+num){
-    console.log(i);
-}
+//var num=Number(prompt('Enter a number:'));
+//
+//for(i=num;i<=num*num;i=i+num){
+//    console.log(i);
+//}
 

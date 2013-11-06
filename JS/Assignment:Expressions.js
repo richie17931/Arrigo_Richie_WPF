c48b6c44c395f8f08ba24e79ea97969611cb2dbc
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-06 14:37:23 -0500

    Assignment:Expressions;SixthCommit

--- JS/Assignment:Expressions.js
+++ JS/Assignment:Expressions.js
@@ -2,12 +2,16 @@
  * Created by richardarrigo on 11/6/13.
  */
 
+var width=Number(prompt('What is the width?'));
+var height=Number(prompt('What is the height?'));
+var value=Number(prompt('Enter a number:'));
+
 
 // Basic Expressions
 var a= 7;
 var b= a+3;
-console.log(a);
-console.log(b);
+console.log("A:" + a);
+console.log("B:" + b);
 
 // Example Expressions
 var yearBorn= 1992;
@@ -16,11 +20,13 @@ console.log('Age:'+age);
 
 // Arithmetic Operators
 // area of a triangle is half the width x height
-var width=Number(prompt('What is the width?'));
-var height=Number(prompt('What is the height?'));
 var area= width*height/2;
-console.log(area);
+console.log("Area:"+area);
 
 // Using Modulus
 var remainder= 101%10;
-console.log(remainder);
+console.log("Remainder:"+remainder);
+
+value=value%2;
+if(value==0){console.log('Your number is even.');}
+else{console.log('Your number is odd');}

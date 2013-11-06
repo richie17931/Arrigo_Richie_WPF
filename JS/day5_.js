9c9bbfc4b8eff413e3d89b0f44f0ab61d0dff53f
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-06 11:26:22 -0500

    Day5SecondCommit

--- JS/day5_.js
+++ JS/day5_.js
@@ -22,9 +22,48 @@
 //var lCalc= -1*(equity-assets);
 //console.log(lCalc+':'+liability);
 
-// prompt user for a number and display that index
-var shoppingList=['Milk','Eggs','Bread','Bacon'];
+//// prompt user for a number and display that index
+//var shoppingList=['Milk','Eggs','Bread','Bacon'];
+//
+//var item=Number(prompt('Which item would you like to select(1-4)?'));
+//console.log(shoppingList[item-1]);
+//
+//// show the array length then add an item
+//// use a for loop to display everything in the array and no more or less than the number of items
+//// add another item and display
+//
+//console.log(shoppingList.length);
+//shoppingList.push('Waffles');
+//console.log(shoppingList.length);
+//
+//for(i=0;i<shoppingList.length;i++){
+//    console.log(shoppingList[i]);
+//}
+//
+//shoppingList.push('Ketchup');
+//
+//for(i=0;i<shoppingList.length;i++){
+//    console.log(shoppingList[i]);
+//}
 
-var item=Number(prompt('Which item would you like to select(1-4)?'));
-console.log(shoppingList[item-1]);
+var storedPromptValue=prompt('E(100%), G(75%), F(30%), P(0 for entire assignment)');
+var percentageFromPrompt= 0;
+var percentageOfPoints= 10;
 
+// .toUpperCase() takes user input as uppercase
+if(storedPromptValue.toUpperCase()=='E')
+{
+    percentageFromPrompt= 1;
+}
+else if(storedPromptValue.toUpperCase()=='G'){
+    percentageFromPrompt= .75;
+}
+else if(storedPromptValue.toUpperCase()== 'F'){
+    percentageFromPrompt= .3;
+}
+else{
+    percentageFromPrompt= 0;
+}
+
+percentageOfPoints*=percentageFromPrompt;
+console.log(percentageOfPoints);

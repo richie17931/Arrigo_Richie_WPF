5de8324041346ab40b043c4248b6ab3b38dc5a32
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-06 15:54:43 -0500

    Assignment:Expressions;EighthCommit

--- JS/Assignment:Expressions.js
+++ JS/Assignment:Expressions.js
@@ -5,7 +5,18 @@
 var width=Number(prompt('What is the width?'));
 var height=Number(prompt('What is the height?'));
 var value=Number(prompt('Enter a number:'));
+var numberOfQuizzes=Number(prompt('How many quiz grades to calculate?'));
 
+var quizzes=[numberOfQuizzes];
+var total= 0;
+for(i=0;i<numberOfQuizzes;i++){
+    var grade=Number(prompt('Enter quiz grade:'));
+    quizzes[i]=grade;
+    total+=quizzes[i];
+}
+
+var length=Number(prompt('Enter the length:'));
+var height=Number(prompt('Enter the height:'));
 
 // Basic Expressions
 var a= 7;
@@ -13,15 +24,18 @@ var b= a+3;
 console.log("A:" + a);
 console.log("B:" + b);
 
+
 // Example Expressions
 var yearBorn= 1992;
 var age= 2013-yearBorn;
 console.log('Age:'+age);
 
+
 // Arithmetic Operators
 // area of a triangle is half the width x height
 var area= width*height/2;
 console.log("Area:"+area);
+
 
 // Using Modulus
 var remainder= 101%10;
@@ -31,6 +45,7 @@ value=value%2;
 if(value==0){console.log('Your number is even.');}
 else{console.log('Your number is odd');}
 
+
 // Order of Operations(PEMDAS)
 var quiz1= 100;
 var quiz2= 95;
@@ -38,6 +53,12 @@ var quiz3= 78;
 var quiz4= 90;
 var quiz5= 93;
 var quiz6= 84;
-
 var average= (quiz1+quiz2+quiz3+quiz4+quiz5+quiz6)/6;
-console.log(average);
+console.log('Quiz Average:'+average);
+
+console.log('Quiz Average:'+total/numberOfQuizzes);
+
+// Perimeter
+console.log('The perimeter= '+((length*2)+(height*2)));
+
+

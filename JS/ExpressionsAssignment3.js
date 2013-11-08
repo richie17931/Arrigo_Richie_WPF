711ef2b81d617d465347d7cada5ea7b27e6e9824
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-08 14:00:25 -0500

    Assignment:Expressions;ThirteenthCommit

---
+++ JS/ExpressionsAssignment3.js
@@ -0,0 +1,10 @@
+/**
+ * Created by richardarrigo on 11/8/13.
+ */
+
+// Wacky Expressions example
+// Video game suggestions
+
+var genre=prompt('What type of game would you like to play?(FPS, RPG, Strategy');
+var type=prompt('Are you a casual or hardcore player?');
+

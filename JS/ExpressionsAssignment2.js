dd92e45b66af08c8edf688973074645d6897c704
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-08 13:35:39 -0500

    Assignment:Expressions;EleventhCommit

---
+++ JS/ExpressionsAssignment2.js
@@ -0,0 +1,10 @@
+/**
+ * Created by richardarrigo on 11/8/13.
+ */
+
+// Industry Expressions example
+// Quiz average
+
+var numberOfQuizzes=Number(prompt('How many quiz grades to calculate?'));
+var quizzes=[numberOfQuizzes];
+var total= 0;

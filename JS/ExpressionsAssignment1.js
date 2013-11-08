f726e7e2451747df3102289595760ac4a98c4dcf
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-08 14:39:54 -0500

    Assignment:Expressions;TwentiethCommit

--- JS/ExpressionsAssignment1.js
+++ JS/ExpressionsAssignment1.js
@@ -6,7 +6,30 @@
 // Personal Expressions example
 // Age Calculator
 
-var yearBorn=Number(prompt('What year were you born?'));
-var currentYear=Number(prompt('What year is it currently?'));
-var age= currentYear-yearBorn;
-console.log('Your age:'+age);
+//var yearBorn=Number(prompt('What year were you born?'));
+//var currentYear=Number(prompt('What year is it currently?'));
+//var age= currentYear-yearBorn;
+//console.log('Your age:'+age);
+
+var birthMonth=Number(prompt('What month were you born?(1-12)'));
+var birthDay=Number(prompt('What day were you born?(1-31)'));
+var birthYear=Number(prompt('What year were you born?'));
+var currentMonth=Number(prompt('What month is it?(1-12)'));
+var currentDay=Number(prompt('What day is it?(1-31)'));
+var currentYear=Number(prompt('What year is it?'));
+var age= 0;
+
+console.log('Your birthday is '+birthMonth+'/'+birthDay+'/'+birthYear);
+if(currentMonth<birthMonth){
+    age= currentYear-birthYear-1;
+    console.log('Your age:'+age);
+}
+else if(currentMonth==birthMonth && currentDay<birthDay){
+    age= currentYear-birthYear-1;
+    console.log('Your age:'+age);
+}
+else{
+    var age= currentYear-birthYear;
+    console.log('Your age:'+age);
+}
+

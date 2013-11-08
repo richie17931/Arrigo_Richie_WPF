09988c6692779f642e047d22a01332f657512cec
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-08 14:01:03 -0500

    Assignment:Expressions;FourteenthCommit

--- JS/ExpressionsAssignment3.js
+++ JS/ExpressionsAssignment3.js
@@ -8,3 +8,14 @@
 var genre=prompt('What type of game would you like to play?(FPS, RPG, Strategy');
 var type=prompt('Are you a casual or hardcore player?');
 
+if(genre=='FPS'||genre=='fps'){
+    if(type=='casual'||type=='Casual'){
+        console.log('I suggest you play Call of Duty.');
+    }
+    else if(type=='hardcore'||type=='Hardcore'){
+        console.log('I suggest you play Battlefield.');
+    }
+    else{
+        console.log('You did not enter a valid selection.');
+    }
+}

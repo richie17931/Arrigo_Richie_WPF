611e7681e8270f289d6ce0977605162b222b2ebc
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-08 13:30:17 -0500

    Assignment:Expressions;TenthCommit

---
+++ JS/ExpressionsAssignment1.js
@@ -0,0 +1,11 @@
+/**
+ * Created by richardarrigo on 11/8/13.
+ */
+
+// Personal Expressions example
+// Age Calculator
+
+var yearBorn=Number(prompt('What year were you born?'));
+var currentYear=Number(prompt('What year is it currently?'));
+var age= currentYear-yearBorn;
+console.log('Your age:'+age);

dd368f0d30ae029091134544c1381914483fc9fd
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-15 15:53:43 -0500

    AssignmentConditionals;CommitOne

---
+++ Conditionals_Personal/Conditionals-Personal.js
@@ -0,0 +1,9 @@
+/**
+ * Created by richardarrigo on 11/15/13.
+ */
+
+//budget calculator
+var budget=Number(prompt('Enter your total budget amount: '));
+var expenses=Number(prompt('Enter your total expenses: '));
+var profit=budget-expenses;
+

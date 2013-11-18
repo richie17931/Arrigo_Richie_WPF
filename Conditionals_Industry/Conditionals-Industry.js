95d75f3ff26b1702d0135bdee1138560f27acd6f
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-18 15:38:38 -0500

    Assignment:Conditionals;CommitTwenty

--- Conditionals_Industry/Conditionals-Industry.js
+++ Conditionals_Industry/Conditionals-Industry.js
@@ -2,7 +2,13 @@
  * Created by richardarrigo on 11/18/13.
  */
 
+// Determine if it is a work day given that you have Sundays off
 
+// user's input for the current day of the week
+var day=prompt('What day is it today?');
+// ternary operator to determine if it is Sunday or a work day
+day=='Sunday'? console.log('You have the day off!') :
+              console.log('You have work today.');
 
 
 

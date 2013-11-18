f2aa23e6ceea183aa6817d074eac3efcd40224ef
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-18 15:04:32 -0500

    Assignment:Conditionals;FourteenthCommit

--- Conditionals_Industry/Conditionals-Industry.js
+++ Conditionals_Industry/Conditionals-Industry.js
@@ -2,3 +2,9 @@
  * Created by richardarrigo on 11/18/13.
  */
 
+// check bags to see if you have enough room to collect more loot
+
+// prompt the user to enter the number of bag slots they have open
+var slots=prompt('How many open slots are in your bag?');
+
+

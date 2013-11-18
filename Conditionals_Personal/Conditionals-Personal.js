5d8d4aeafdaf0d537f42a99074ea01aee9028252
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-18 13:52:56 -0500

    Assignment:Conditionals;SeventhCommit

--- Conditionals_Personal/Conditionals-Personal.js
+++ Conditionals_Personal/Conditionals-Personal.js
@@ -11,12 +11,15 @@ var expenses=Number(prompt('Enter your total expenses: '));
 //calculates the amount of money left after bills are paid
 var profit=budget-expenses;
 
+// to check if the user has money left over after bills are paid
 if(profit>0){
     console.log('You have $'+profit+' extra to spend!');
 }
+// check if the user has exactly $0.00 left over after paying bills
 else if(profit==0){
     console.log('Your bills are paid but you have no extra money!');
 }
+// check if the user is in debt after paying their bills
 else{
     console.log('You are $'+Math.abs(profit)+' in debt!');
 }

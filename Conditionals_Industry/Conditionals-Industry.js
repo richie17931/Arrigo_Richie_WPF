f55a9068a1e033dd9c86c211c525f9697c6bf612
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-18 15:12:57 -0500

    Assignment:Conditionals;SixteenthCommit

--- Conditionals_Industry/Conditionals-Industry.js
+++ Conditionals_Industry/Conditionals-Industry.js
@@ -4,10 +4,14 @@
 
 // check bags to see if you have enough room to collect more loot
 
-// prompt the user to enter the number of bag slots they have open
+// prompt the user to enter the number of bag slots they have total
 var slots=prompt('How many open slots are in your bag?');
 // create an array to represent the backpack
 var backpack=[slots];
+// variable to check when to quit asking the user for input
+var quitTest=true;
+
+
 
 
 

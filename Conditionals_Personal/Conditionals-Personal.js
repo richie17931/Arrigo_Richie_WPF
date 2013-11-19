dfab6484a67a9108224d63346f369aa06f84698c
richie17931 <richardarrigo@Richards-MacBook-Pro-8.local>
2013-11-19 18:17:08 -0500

    Assignment:Conditionals;CommitTwentyThree
    budget displays decimals to two places

--- Conditionals_Personal/Conditionals-Personal.js
+++ Conditionals_Personal/Conditionals-Personal.js
@@ -8,12 +8,12 @@
 // do while loop to run until the user enters a valid amount for their budget
 do{
     //the total amount of money the user has
-    var budget=Number(prompt('Enter your total budget amount: '));
+    var budget=parseFloat(prompt('Enter your total budget amount: '));
 } while(isNaN(budget)==true)
 // do while loop to run until the user enters a valid amount for their expenses
 do{
     //the total amount of the users bills and expenses
-    var expenses=Number(prompt('Enter your total expenses: '));
+    var expenses=parseFloat(prompt('Enter your total expenses: '));
 } while(isNaN(expenses)==true)
 
 //calculates the amount of money left after bills are paid
@@ -21,7 +21,7 @@ var profit=budget-expenses;
 
 // to check if the user has money left over after bills are paid
 if(profit>0){
-    console.log('You have $'+profit+' extra to spend!');
+    console.log('You have $'+profit.toFixed(2)+' extra to spend!');
 }
 // check if the user has exactly $0.00 left over after paying bills
 else if(profit==0){
@@ -29,5 +29,5 @@ else if(profit==0){
 }
 // check if the user is in debt after paying their bills
 else{
-    console.log('You are $'+Math.abs(profit)+' in debt!');
+    console.log('You are $'+Math.abs(profit.toFixed(2))+' in debt!');
 }

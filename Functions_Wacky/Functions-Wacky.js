7070ee6694ea88e0be97d835a8c81405abf6b0f0
richie17931 <richardarrigo@Richards-MacBook-Pro-9.local>
2013-11-20 15:42:06 -0500

    Assignment:Functions;CommitEleven

--- Functions_Wacky/Functions-Wacky.js
+++ Functions_Wacky/Functions-Wacky.js
@@ -4,5 +4,14 @@
 
 // Richie Arrigo | 2492145 | 11/20/13
 
-// create a backpack to store the user's items
-// required parameters: backpack slots
+// create a backpack with twenty slots to store the user's items
+// required parameters: user's items
+
+var slots= Number(prompt('How many items do you have to store?'));
+var backpack= [];
+
+function inventory(slots){
+    for(i=0; i<slots; i++){
+        backpack[i]= prompt('Enter an item to add: ');
+    }
+}

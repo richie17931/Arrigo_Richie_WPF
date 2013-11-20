14ed23dd846ec3a329bde65ba8a61d546217e20b
richie17931 <richardarrigo@Richards-MacBook-Pro-9.local>
2013-11-20 15:44:06 -0500

    Assignment:Functions;CommitTwelve

--- Functions_Wacky/Functions-Wacky.js
+++ Functions_Wacky/Functions-Wacky.js
@@ -7,11 +7,16 @@
 // create a backpack with twenty slots to store the user's items
 // required parameters: user's items
 
+// variable to find out how many items the user needs to store
 var slots= Number(prompt('How many items do you have to store?'));
+// array to hold the contents of the user's inventory
 var backpack= [];
 
+// function body that takes in the number of items the user has
 function inventory(slots){
+    // loop the number of times you need to add an item
     for(i=0; i<slots; i++){
         backpack[i]= prompt('Enter an item to add: ');
     }
+    console.log(backpack);
 }

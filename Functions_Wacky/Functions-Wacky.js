467457418d5d28d6b094d4fd9b72e9fd6d8cf379
richie17931 <richardarrigo@Richards-MacBook-Pro-9.local>
2013-11-20 15:53:42 -0500

    Assignment:Functions;CommitFourteen

--- Functions_Wacky/Functions-Wacky.js
+++ Functions_Wacky/Functions-Wacky.js
@@ -8,17 +8,21 @@
 // required parameters: user's items
 
 // variable to find out how many items the user needs to store
-var slots= Number(prompt('How many items do you have to store?'));
+do{
+    var slots= Number(prompt('How many items do you have to store?'));
+} while(isNaN(slots)==true)
 // array to hold the contents of the user's inventory
 var backpack= [];
 
 // function body that takes in the number of items the user has
-function inventory(slots){
+function inventory(s){
     // loop the number of times you need to add an item
-    for(i=0; i<slots; i++){
+    for(i=0; i<s; i++){
         // add the item that the user enters into their backpack
         backpack[i]= prompt('Enter an item to add: ');
     }
     // display the contents of their pack
-    console.log(backpack);
+    console.log('Your backpack contains: '+backpack);
 }
+
+inventory(slots);

cf53c5eb4757b6ec3a03724f75b795d12af8529e
richie17931 <richardarrigo@Richards-MacBook-Pro-9.local>
2013-11-20 13:59:35 -0500

    Assignment:Conditionals;CommitTwo

--- Functions_Industry/Functions-Industry.js
+++ Functions_Industry/Functions-Industry.js
@@ -3,3 +3,10 @@
  */
 
 // Richie Arrigo | 2492145 | 11/20/13
+// calculator for the area of a trapezoid in order to cover the surface area with tiles
+// required parameters: base1, base2, height
+
+var baseOne= Number(prompt('What is the length of base one(B1)?'));
+var baseTwo= Number(prompt('What is the length of base two(B2)?'));
+var height= Number(prompt('What is the height?'));
+

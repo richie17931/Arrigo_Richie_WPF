8e68712e22c546bb0bb09d05d5f88a5d33ee62af
richie17931 <richardarrigo@Richards-MacBook-Pro-9.local>
2013-11-20 14:51:58 -0500

    Assignment:Functions;CommitSix

--- Functions_Industry/Functions-Industry.js
+++ Functions_Industry/Functions-Industry.js
@@ -3,16 +3,22 @@
  */
 
 // Richie Arrigo | 2492145 | 11/20/13
-    
+
 // calculator for the area of a trapezoid(in feet) in order to cover the surface area with tiles
 // required parameters: base1, base2, height
 
 // create a variable to hold the prompted value for base one of the trapezoid
-var baseOne= Number(prompt('What is the length of base one(B1)?'));
+do{
+    var baseOne= parseFloat(prompt('What is the length of base one(B1)?'));
+} while(isNaN(baseOne)==true)
 // create a variable to hold the prompted value for base two of the trapezoid
-var baseTwo= Number(prompt('What is the length of base two(B2)?'));
+do{
+    var baseTwo= parseFloat(prompt('What is the length of base two(B2)?'));
+} while(isNaN(baseTwo)==true)
 // create a variable to hold the prompted value for the height of the trapezoid
-var height= Number(prompt('What is the height?'));
+do{
+    var height= parseFloat(prompt('What is the height?'));
+} while(isNaN(height)==true)
 
 // create a function body
 function trapezoidAreaCalc(b1,b2,h){
--- Functions_Personal/Functions-Personal.js
+++ Functions_Personal/Functions-Personal.js
@@ -3,3 +3,14 @@
  */
 
 // Richie Arrigo | 2492145 | 11/20/13
+
+// calculator to find the area or volume of a sphere depending on the user's input
+// required parameters: radius, calculation
+
+// create a variable to hold the radius of the sphere
+var radius= parseFloat(prompt('What is the radius of the sphere?'));
+// create a loop to run until the user inputs what they want to calculate(area or volume)
+do{
+    var calculation= prompt('Would you like to find the area or volume?');
+} while(calculation.toUpperCase()!= 'AREA' && calculation.toUpperCase()!= 'VOLUME')
+

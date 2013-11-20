f9e35e685658300be8d3dbf85cc11d4e0b882e46
richie17931 <richardarrigo@Richards-MacBook-Pro-9.local>
2013-11-20 15:03:12 -0500

    Assignment:Functions;CommitSeven

--- Functions_Personal/Functions-Personal.js
+++ Functions_Personal/Functions-Personal.js
@@ -14,3 +14,12 @@ do{
     var calculation= prompt('Would you like to find the area or volume?');
 } while(calculation.toUpperCase()!= 'AREA' && calculation.toUpperCase()!= 'VOLUME')
 
+// create a function that takes in the radius and what they want to calculate
+function sphereCalc(rad, calc){
+    var area= calc.toUpperCase()=='AREA'? 4*Math.PI*(rad*rad): (4/3)*Math.PI*(rad*rad*rad)
+    return area;
+}
+
+var userArea= sphereCalc(radius, calculation);
+console.log(userArea.toFixed(3));
+

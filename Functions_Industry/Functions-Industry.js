ab25790ebb8da4a91d4f670c9a92bad3cc135022
richie17931 <richardarrigo@Richards-MacBook-Pro-9.local>
2013-11-20 14:02:24 -0500

    Assignment:Functions;CommitThree

--- Functions_Industry/Functions-Industry.js
+++ Functions_Industry/Functions-Industry.js
@@ -6,7 +6,19 @@
 // calculator for the area of a trapezoid in order to cover the surface area with tiles
 // required parameters: base1, base2, height
 
+// create a variable to hold the prompted value for base one of the trapezoid
 var baseOne= Number(prompt('What is the length of base one(B1)?'));
+// create a variable to hold the prompted value for base two of the trapezoid
 var baseTwo= Number(prompt('What is the length of base two(B2)?'));
+// create a variable to hold the prompted value for the height of the trapezoid
 var height= Number(prompt('What is the height?'));
+
+// create a function body
+function trapezoidAreaCalc(b1,b2,h){
+
+}
+
+
+
+
 
